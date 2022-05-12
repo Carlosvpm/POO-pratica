6200e3c7e54031af0badd0918bf77b9a9b6b579f
Carlos Moraes <moraesvinicius_2002@hotmail.com>
2022-05-12 19:33:44 -0300

    refatorando métodos

--- questao01/Questao01.java
+++ questao01/Questao01.java
@@ -1,39 +1,44 @@
 package questao01;
 
-
 import java.util.InputMismatchException;
 import java.util.Scanner;
-
 
 public class Questao01 {
   static Pessoa[] pessoas = new Pessoa[3];
   static Scanner sc = new Scanner(System.in);
 
-  private static void recebeNome(int i) throws InputMismatchException {
+  private static void recebeNome(int i, Pessoa[] pessoas) throws InputMismatchException {
     System.out.println("DIGITE O NOME DA PESSOA " + (i + 1));
     pessoas[i].setNome(sc.nextLine());
   }
 
-  private static void recebeIdade(int i) throws InputMismatchException {
+  private static void recebeIdade(int i, Pessoa[] pessoas) throws InputMismatchException {
     System.out.println("DIGITE A IDADE DA PESSOA " + (i + 1));
     pessoas[i].setIdade(sc.nextInt());
   }
 
-  private static void recebeCpf(int i) throws InputMismatchException {
+  private static void recebeCpf(int i, Pessoa[] pessoas) throws InputMismatchException {
     System.out.println("DIGITE O CPF DA PESSOA " + (i + 1));
     pessoas[i].setCpf(sc.nextInt());
   }
 
-  public static void main(String args[]) {
+  private static void lerPessoa(int i, Pessoa[] pessoas) {
+    recebeNome(i, pessoas);
+    recebeIdade(i, pessoas);
+    recebeCpf(i, pessoas);
+  }
 
+  private static void mostrarPessoas(int i) {
+    System.out.println("NOME DA PESSOA" + (i + 1) + ": " + pessoas[i].getNome());
+    System.out.println("IDADE DA PESSOA" + (i + 1) + ": " + pessoas[i].getIdade());
+    System.out.println("CPF DA PESSOA" + (i + 1) + ": " + pessoas[i].getCpf());
+  }
+
+  public static void main(String args[]) {
     for (int i = 0; i < 3; i++) {
       pessoas[i] = new Pessoa();
-
       try {
-        recebeNome(i);
-        recebeIdade(i);
-        recebeCpf(i);
-
+        lerPessoa(i, pessoas);
       } catch (InputMismatchException e) {
         System.out.println("Os campos em questão têm tipos específicos, por favor preencha de acordo!");
         i -= 1;
@@ -41,10 +46,7 @@ public class Questao01 {
       sc.nextLine();
     }
     for (int i = 0; i < 3; i++) {
-      System.out.println("NOME DA PESSOA" + (i + 1) + ":" + pessoas[i].getNome());
-      System.out.println("IDADE DA PESSOA" + (i + 1) + ":" + pessoas[i].getIdade());
-      System.out.println("CPF DA PESSOA" + (i + 1) + ":" + pessoas[i].getCpf());
-
+      mostrarPessoas(i);
     }
   }
 
